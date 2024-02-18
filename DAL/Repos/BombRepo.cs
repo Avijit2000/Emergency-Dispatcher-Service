36b723e1ae54cfbcc0af2e8d0b90ccd24b9a0556
Avijit Sarker Tonmoy <71463366+Avijit2000@users.noreply.github.com>
2024-02-19 02:14:50 +0600

    Add files via upload

---
+++ DAL/Repos/BombRepo.cs
@@ -0,0 +1,44 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class BombRepo : Repo, IRepo<bombdisposal, int, bool>
+    {
+        public bool Add(bombdisposal obj)
+        {
+            db.bombdisposals.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var ext = db.bombdisposals.Find(id);
+            db.bombdisposals.Remove(ext);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<bombdisposal> Get()
+        {
+            return db.bombdisposals.ToList();
+        }
+
+        public bombdisposal Get(int id)
+        {
+            return db.bombdisposals.Find(id);
+        }
+
+        public bool Update(bombdisposal obj)
+        {
+            var d = db.bombdisposals.FirstOrDefault(x => x.id == obj.id);
+            d.name = obj.name;
+            d.address = obj.address;
+            return db.SaveChanges() > 0;
+        }
+    }
+}
---
+++ DAL/Repos/PoliceRepo.cs
@@ -0,0 +1,44 @@
+﻿using DAL.EF;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos
+{
+    internal class PoliceRepo : Repo, IRepo<police, int, bool>
+    {
+        public bool Add(police obj)
+        {
+            db.police.Add(obj);
+            return db.SaveChanges() > 0;
+        }
+
+        public bool Delete(int id)
+        {
+            var ext = db.police.Find(id);
+            db.police.Remove(ext);
+            return db.SaveChanges() > 0;
+        }
+
+        public List<police> Get()
+        {
+            return db.police.ToList();
+        }
+
+        public police Get(int id)
+        {
+            return db.police.Find(id);
+        }
+
+        public bool Update(police obj)
+        {
+            var d = db.police.FirstOrDefault(x => x.id == obj.id);
+            d.name = obj.name;
+            d.address = obj.address;
+            return db.SaveChanges() > 0;
+        }
+    }
+}
